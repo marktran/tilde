3fe3a95c57d32a8ed8786d3b53731f7f136c874d
Mark Tran <mark.tran@gmail.com>
2013-05-20 18:40:36 -0700

    LESS flag

--- .config/fish/exports.fish
+++ .config/fish/exports.fish
@@ -1,10 +1,11 @@
 set -x ALTERNATE_EDITOR ''
 set -x CLICOLOR 1
-set -x LSCOLORS 'cxfxcxdxbxegedabagacad'
+set -x LESS -R
+set -x LSCOLORS cxfxcxdxbxegedabagacad
 
 prepend_to_path "$HOME/bin"
 prepend_to_path "/usr/local/bin"
 prepend_to_path "/usr/local/share/npm/bin"
 prepend_to_path "$HOME/.rbenv/shims"
 
-rbenv rehash >/dev/null ^&1
+rbenv rehash > /dev/null ^&1
