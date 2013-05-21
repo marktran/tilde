c9d918e31c597f72af8f85a1773df07db1cd09ea
Mark Tran <mark.tran@gmail.com>
2013-05-20 18:12:51 -0700

    color customizations

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -1,6 +1,8 @@
 set fish_color_error normal
 set fish_color_command 99ad6a
 set fish_color_param fad07a
+set fish_color_quote de5577
+set fish_color_redirection 8fbfdc
 set fish_color_valid_path normal
 set fish_pager_color_prefix fad07a
-set fish_pager_color_progress 555
+set fish_pager_color_progress fad07a
