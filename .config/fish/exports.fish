fc4efcd3d4cb8dee27cb9c65e49ed56c0bb3c4f4
Mark Tran <mark.tran@gmail.com>
2013-06-20 15:04:58 -0700

    fuck that prepend_to_path shit

--- .config/fish/exports.fish
+++ .config/fish/exports.fish
@@ -2,9 +2,4 @@ set -x ALTERNATE_EDITOR ''
 set -x LESS -R
 set -x LS_COLORS di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31
 set -x LSCOLORS cxfxcxdxbxegedabagacad
-
-prepend_to_path "$HOME/bin"
-prepend_to_path "/usr/local/bin"
-prepend_to_path "/usr/local/share/npm/bin"
-prepend_to_path "$HOME/.rbenv/shims"
-deduplicate PATH
+set -x PATH ./bin $HOME/.rbenv/shims /usr/local/share/npm/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
--- .config/fish/functions/prepend_to_path.fish
+++
@@ -1,5 +0,0 @@
-function prepend_to_path -d "Prepend the given directory, if it exists, to \$PATH"
-  if test -d $argv[1]
-    set -gx PATH "$argv[1]" $PATH
-  end
-end
