08d8af5756b6264b3f8b81b2bef7b4adcf4e03a1
Mark Tran <mark.tran@gmail.com>
2016-05-06 03:40:32 +0700

    update exports

--- .config/fish/exports.fish
+++ .config/fish/exports.fish
@@ -1,6 +1,7 @@
 set -x ALTERNATE_EDITOR ''
 set -x EDITOR 'vim'
+set -x HOMEBREW_NO_ANALYTICS 1
 set -x LESS -R
 set -x LS_COLORS di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31
 set -x LSCOLORS cxfxcxdxbxegedabagacad
-set -x PATH ./bin $HOME/bin $HOME/.rbenv/shims /usr/local/share/npm/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+set -x PATH ./bin $HOME/bin /Applications/Postgres.app/Contents/Versions/9.3/bin $HOME/.rbenv/shims /usr/local/share/npm/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
