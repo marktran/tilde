5e474065f258ae750f54e33b65082098ccea87eb
Mark Tran <mark.tran@gmail.com>
2016-05-22 01:07:58 +0700

    use abbr

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,14 +1,13 @@
-alias be 'bundle exec'
+abbr -a b bundle
 alias browse 'hub browse'
 alias c 'calc -d'
 alias e 'emacsclient -nw -c'
-alias g 'git'
+abbr -a g git
 alias ip 'dig +short myip.opendns.com @resolver1.opendns.com'
 alias l 'ls'
 alias l. 'ls -d .*'
 alias ll 'ls -l'
-alias m 'mplayer'
-alias s 'ssh'
+abbr -a s ssh
 alias t 'tree'
 
 alias irssi "ssh mack -t 'screen -r irssi'"
