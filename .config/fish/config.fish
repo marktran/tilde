58e6713b73662128a32624c8e030d525483a7f81
Mark Tran <mark.tran@gmail.com>
2016-05-22 17:41:11 +0700

    source colors.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 source "$HOME/.config/fish/exports.fish"
 source "$HOME/.config/fish/aliases.fish"
+source "$HOME/.config/fish/colors.fish"
 
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
