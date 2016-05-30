0fd8662c041f156bf5f5f392d6d9e33bfe20b32a
Mark Tran <mark.tran@gmail.com>
2016-05-31 06:39:12 +0700

    use test

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,5 @@ source "$HOME/.config/fish/exports.fish"
 source "$HOME/.config/fish/aliases.fish"
 source "$HOME/.config/fish/colors.fish"
 
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-
+test -f /usr/local/share/autojump/autojump.fish; and source /usr/local/share/autojump/autojump.fish
 status --is-interactive; and type -q rbenv; and source (rbenv init -|psub)
