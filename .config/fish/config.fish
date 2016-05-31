7a79d57c4a53a0c174dcc2678505f19278701a36
Mark Tran <mark.tran@gmail.com>
2016-05-31 18:04:39 +0700

    run rbenv init for non-interactive shells

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,4 @@ source "$HOME/.config/fish/aliases.fish"
 source "$HOME/.config/fish/colors.fish"
 
 test -f /usr/local/share/autojump/autojump.fish; and source /usr/local/share/autojump/autojump.fish
-status --is-interactive; and type -q rbenv; and source (rbenv init -|psub)
+type -q rbenv; and source (rbenv init -|psub)
