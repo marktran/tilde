b287d04b44b50ea40599e715d48a1f6a462f9a0b
Mark Tran <mark.tran@gmail.com>
2018-02-12 02:23:33 +0700

    move exports around

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,10 @@
-source "$HOME/.config/fish/exports.fish"
 source "$HOME/.config/fish/aliases.fish"
+source "$HOME/.config/fish/exports.fish"
 source "$HOME/.config/fish/colors.fish"
 
 test -f /usr/local/share/autojump/autojump.fish; and source /usr/local/share/autojump/autojump.fish
 type -q direnv; and eval (direnv hook fish)
 type -q rbenv; and source (rbenv init -|psub)
+
+# do this after initializing rbenv
+set -x PATH ./bin $PATH
