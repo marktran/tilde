4637a09853847445f9a8cc256ef097075d6d37b7
Mark Tran <mark.tran@gmail.com>
2018-03-05 16:46:40 +0700

    source local.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ source "$HOME/.config/fish/aliases.fish"
 source "$HOME/.config/fish/exports.fish"
 source "$HOME/.config/fish/colors.fish"
 
+test -e local.fish; and source local.fish
+
 test -f /usr/local/share/autojump/autojump.fish; and source /usr/local/share/autojump/autojump.fish
 type -q direnv; and eval (direnv hook fish)
 type -q rbenv; and source (rbenv init -|psub)
