4214bb36280795d1effa3cd4085c8f689f24e99e
Mark Tran <mark.tran@gmail.com>
2019-08-22 14:27:38 -0400

    switch to Volta

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,8 @@ type -q direnv; and eval (direnv hook fish)
 
 # do this after initializing rbenv
 set -x PATH ./bin $PATH
+
+set -gx VOLTA_HOME "$HOME/.volta"
+test -s "$VOLTA_HOME/load.fish"; and source "$VOLTA_HOME/load.fish"
+
+string match -r ".volta" "$PATH" > /dev/null; or set -gx PATH "$VOLTA_HOME/bin" $PATH
