c3cc1ffab689e0273223a9a155e40d16a79c9b07
Mark Tran <mark.tran@gmail.com>
2019-12-20 20:33:04 -0500

    add vscode config

---
+++ vscode/extensions.sh
@@ -0,0 +1,19 @@
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension apollographql.vscode-apollo
+code --install-extension arcticicestudio.nord-visual-studio-code
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension EditorConfig.EditorConfig
+code --install-extension eg2.vscode-npm-script
+code --install-extension esbenp.prettier-vscode
+code --install-extension mischah.relaxed-theme
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension Orta.vscode-jest
+code --install-extension pawelgrzybek.gatito-theme
+code --install-extension taniarascia.new-moon-vscode
+code --install-extension Tyriar.sort-lines
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscodevim.vim
