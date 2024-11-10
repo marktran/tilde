7662d7b55e11ffe141a56576157783844fc54213
Mark Tran <mark.tran@gmail.com>
2024-11-09 18:58:41 -0800

    add aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -9,10 +9,12 @@ alias ip 'dig +short myip.opendns.com @resolver1.opendns.com'
 alias l 'ls'
 alias l. 'ls -d .*'
 alias ll 'ls -l'
+alias q 'llm -s "Answer in as few words as possible. Use a brief style with short replies." -m llama3.1:8b'
 abbr -a s ssh
 alias t 'tree'
 abbr -a tf terraform
 abbr -a mux tmuxinator
+alias writer 'open $1 -a /Applications/iA\ Writer.app'
 abbr -a y yarn
 
 type -q gcal; and alias cal gcal
