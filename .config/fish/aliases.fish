a2bca6a041468589e965fbc8c633583309d80b1b
Mark Tran <mark.tran@gmail.com>
2024-11-16 17:59:29 -0800

    add better l. function

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -7,7 +7,6 @@ abbr -a g git
 abbr -a h heroku
 alias ip 'dig +short myip.opendns.com @resolver1.opendns.com'
 alias l 'ls'
-alias l. 'ls -d .*'
 alias ll 'ls -l'
 alias q 'llm -s "Answer in as few words as possible. Use a brief style with short replies." -m llama3.1:8b'
 abbr -a s ssh
---
+++ .config/fish/functions/l..fish
@@ -0,0 +1,4 @@
+function l.
+  set -l files .*
+  set -q files[1]; and ls -d $files
+end
