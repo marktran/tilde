41acb9645d37d36bf47024ab152cbb3d7c7c44f0
Mark Tran <mark.tran@gmail.com>
2025-08-23 19:13:17 -0700

    update fish aliases

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,13 +1,12 @@
 abbr -a b brew
 abbr -a be bundle exec
-alias browse 'hub browse'
 alias c 'calc -d'
 alias e 'emacsclient -nw -c'
 abbr -a g git
-abbr -a h heroku
 alias ip 'dig +short myip.opendns.com @resolver1.opendns.com'
 alias l 'ls'
 alias ll 'ls -l'
+abbr -a n 'nvim'
 alias q 'llm -s "Answer in as few words as possible. Use a brief style with short replies." -m llama3.1:8b'
 abbr -a s ssh
 alias t 'tree'
