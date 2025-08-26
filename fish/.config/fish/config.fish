e1dc969e06569e2f72538353fa94f5716ee0bde2
Mark Tran <mark.tran@gmail.com>
2025-08-26 10:45:08 -0700

    refactor config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,15 +6,11 @@ test -e local.fish; and source local.fish
 
 zoxide init --cmd j fish | source
 type -q mise; and mise activate fish | source
-not type -q mise; and type -q brew; and source (brew --prefix mise)/share/fish/vendor_conf.d/mise-activate.fish
 type -q direnv; and eval (direnv hook fish)
-
-# do this after initializing rbenv
-set -x PATH ./bin $PATH
 
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+if test (uname) = Darwin
+    source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+end
