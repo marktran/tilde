11f5b6a083f8a9d311320544a8da63924bc6e888
Mark Tran <mark@Marks-MacBook-Air-2.local>
2025-08-25 17:44:32 -0700

    conditionally set vars for macOS

--- fish/.config/fish/exports.fish
+++ fish/.config/fish/exports.fish
@@ -1,9 +1,15 @@
 set -x ALTERNATE_EDITOR ''
 set -x EDITOR nvim
-set -x HOMEBREW_NO_ANALYTICS 1
 set -x LESS -R
 set -x LS_COLORS di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31
 set -x LSCOLORS cxfxcxdxbxegedabagacad
 set -x PAGER less
-set -x PATH ./node_modules/.bin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin > /dev/null 2>&1
+set -x PATH ./node_modules/.bin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin > /dev/null 2>&1
 set -x _ZO_ECHO 1
+
+if test (uname) = "Darwin"
+    set -x CPATH /opt/homebrew/include $CPATH
+    set -x HOMEBREW_NO_ANALYTICS 1
+    set -x LIBRARY_PATH /opt/homebrew/lib/gcc/14 $LIBRARY_PATH
+    set -x PATH /opt/homebrew/bin
+end
