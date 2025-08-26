00dc0fd0e04185f788f0a7d4c9708b71ae6229e8
Mark Tran <mark@Marks-MacBook-Air-2.local>
2025-08-25 17:39:29 -0700

    update PATH

--- fish/.config/fish/exports.fish
+++ fish/.config/fish/exports.fish
@@ -5,5 +5,5 @@ set -x LESS -R
 set -x LS_COLORS di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31
 set -x LSCOLORS cxfxcxdxbxegedabagacad
 set -x PAGER less
-set -x PATH ./node_modules/.bin $HOME/.cargo/bin $HOME/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin >/dev/null 2>&1
+set -x PATH ./node_modules/.bin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin > /dev/null 2>&1
 set -x _ZO_ECHO 1
