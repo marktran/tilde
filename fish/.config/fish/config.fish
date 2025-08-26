72354c1bcafb642f4e0c1bab0f973be4e125a8da
Mark Tran <mark@Marks-MacBook-Air-2.local>
2025-08-25 18:21:09 -0700

    add OrbStack config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,3 +14,7 @@ set -x PATH ./bin $PATH
 
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
