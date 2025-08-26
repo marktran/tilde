497e9a75026eae8261b3f5a0e62fa227025a96b4
Mark Tran <mark@Marks-MacBook-Air-2.local>
2025-08-25 18:20:54 -0700

    refactor exports for macOS

--- fish/.config/fish/exports.fish
+++ fish/.config/fish/exports.fish
@@ -1,15 +1,14 @@
-set -x ALTERNATE_EDITOR ''
-set -x EDITOR nvim
-set -x LESS -R
-set -x LS_COLORS di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31
-set -x LSCOLORS cxfxcxdxbxegedabagacad
-set -x PAGER less
-set -x PATH ./node_modules/.bin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin > /dev/null 2>&1
-set -x _ZO_ECHO 1
+set -gx ALTERNATE_EDITOR ''
+set -gx EDITOR nvim
+set -gx LESS -R
+set -gx LS_COLORS di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31
+set -gx LSCOLORS cxfxcxdxbxegedabagacad
+set -gx PAGER less
+set -gx PATH ./node_modules/.bin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin
+set -gx _ZO_ECHO 1
 
-if test (uname) = "Darwin"
-    set -x CPATH /opt/homebrew/include $CPATH
-    set -x HOMEBREW_NO_ANALYTICS 1
-    set -x LIBRARY_PATH /opt/homebrew/lib/gcc/14 $LIBRARY_PATH
-    set -x PATH /opt/homebrew/bin
+if test (uname) = Darwin
+    set -gx CPATH /opt/homebrew/include $CPATH
+    set -gx HOMEBREW_NO_ANALYTICS 1
+    set -gx LIBRARY_PATH /opt/homebrew/lib/gcc/14 $LIBRARY_PATH
 end
