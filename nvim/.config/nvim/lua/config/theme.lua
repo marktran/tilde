82782c4c7d9a8e1841c5bed217698176c14b15ec
Mark Tran <mark.tran@gmail.com>
2025-08-30 04:20:45 -0700

    reconfigure rose-pine with transparency

--- nvim/.config/nvim/lua/config/theme.lua
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -1,1 +1,8 @@
-vim.cmd("colorscheme rose-pine")
+local hl = function(name, opts)
+  vim.api.nvim_set_hl(0, name, opts)
+end
+
+require("rose-pine").setup({
+  styles = { transparency = true },
+})
+vim.cmd.colorscheme("rose-pine")
--- nvim/.config/nvim/lua/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/plugins/rose-pine.lua
@@ -1,15 +1,4 @@
 return {
   "rose-pine/neovim",
   name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      extend_background_behind_borders = true,
-
-      styles = {
-        bold = true,
-        italic = true,
-        tranparency = false,
-      }
-    })
-  end
 }
