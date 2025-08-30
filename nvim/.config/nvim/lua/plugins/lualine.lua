30986159bfbad037e4110c0a74a7e4f0cfea6633
Mark Tran <mark.tran@gmail.com>
2025-08-30 00:56:28 -0700

    make status line global

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,7 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = false,
+        globalstatus = true,
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
       },
