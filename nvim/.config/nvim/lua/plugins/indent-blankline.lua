525a6543b0d58370a1302b8ae22153c7901fbee6
Mark Tran <mark.tran@gmail.com>
2025-08-30 03:43:39 -0700

    install indent-blankline

---
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    indent = {
+      char = "⋅",   -- pick your dot style: "·", "⋅", "•"
+    },
+    scope = { enabled = false },
+  },
+}
