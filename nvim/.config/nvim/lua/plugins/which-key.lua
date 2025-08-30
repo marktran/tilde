08eb7e12c99a37bdfa2d9b7c83faeaeaaa194a29
Mark Tran <mark.tran@gmail.com>
2025-08-30 00:40:51 -0700

    rework which-key

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -3,8 +3,13 @@ return {
   event = "VeryLazy",
 
   config = function()
-    local wk = require("which-key")
+    require("which-key").setup({
+      icons = {
+        mappings = false,
+      },
+    })
 
+    local wk = require("which-key")
     wk.add({
       { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
       { "<leader><leader>", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
@@ -17,11 +22,10 @@ return {
       { "<leader>s", group = "Search" },
       { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
 
-      { "<leader>q", group = "Session" },
-      { "<leader>qQ", "<cmd>qa!<cr>", desc = "Quit All" },
-      { "<leader>qq", "<cmd>q<cr>", desc = "Quit" },
-      { "<leader>qw", "<cmd>w<cr>", desc = "Save" },
-      { "<leader>qx", "<cmd>x<cr>", desc = "Save & Quit" },
+      { "<leader>N", group = "Session" },
+      { "<leader>NQ", "<cmd>qa!<cr>", desc = "Quit All" },
+      { "<leader>Nw", "<cmd>w<cr>", desc = "Save" },
+      { "<leader>Nx", "<cmd>x<cr>", desc = "Save & Quit" },
     })
   end,
 }
