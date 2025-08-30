e9505f9eaff6c1cc3dde06b4580f9e6fa7f433f8
Mark Tran <mark.tran@gmail.com>
2025-08-30 01:06:34 -0700

    disable signs in neogit

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -3,6 +3,13 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim", -- optional
+    "nvim-telescope/telescope.nvim",
   },
+
+  config = function()
+    require("neogit").setup({
+      disable_hint = true,
+      disable_signs = true,
+    })
+  end
 }
