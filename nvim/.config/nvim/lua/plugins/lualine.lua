4eeadf65fa3355a333a38fd1986d700a38751da7
Mark Tran <mark.tran@gmail.com>
2025-08-30 00:41:20 -0700

    add lualine

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        icons_enabled = false,
+      },
+
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
+    })
+    end
+  }
