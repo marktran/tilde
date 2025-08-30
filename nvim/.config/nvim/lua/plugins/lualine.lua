c80c27be66893ecaa735acd27f65e0ee45ea6ba1
Mark Tran <mark.tran@gmail.com>
2025-08-30 00:55:43 -0700

    flatten status line

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,8 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = false,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
       },
 
       sections = {
