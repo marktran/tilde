3a577b4eaf6af6e536b41161b9fe1914bbc8348a
Mark Tran <mark.tran@gmail.com>
2025-08-30 23:37:40 -0700

    disable lualine on snacks dashboard

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
+        disabled_filetypes = { "snacks_dashboard" },
         icons_enabled = false,
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
