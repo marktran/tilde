3f8dd845241163207fd91ab546ffb8caa838c37d
Mark Tran <mark.tran@gmail.com>
2026-02-27 22:19:42 -0800

    fish: remove unused shell aliases

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,18 +1,12 @@
 abbr -a b brew
-abbr -a be bundle exec
 alias c 'calc -d'
-alias e 'emacsclient -nw -c'
 abbr -a g git
 alias ip 'dig +short myip.opendns.com @resolver1.opendns.com'
 alias l 'ls'
 alias ll 'ls -l'
 abbr -a n 'nvim'
-alias q 'llm -s "Answer in as few words as possible. Use a brief style with short replies." -m llama3.1:8b'
 abbr -a s sesh
 abbr -a t tmux
-abbr -a mux tmuxinator
-alias writer 'open $1 -a /Applications/iA\ Writer.app'
 abbr -a vi nvim
-abbr -a y yarn
 
 type -q gcal; and alias cal gcal
