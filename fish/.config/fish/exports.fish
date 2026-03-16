fa1e992b1856b6001625ab0c567c79d2bfa349a7
Mark Tran <mark.tran@gmail.com>
2026-03-15 18:15:58 -0700

    fix(fish): gate Omarchy path to Linux

--- fish/.config/fish/exports.fish
+++ fish/.config/fish/exports.fish
@@ -7,6 +7,11 @@ set -gx PAGER less
 set -gx PATH ./node_modules/.bin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin
 set -gx _ZO_ECHO 1
 
+if test (uname) = Linux; and test -d $HOME/.local/share/omarchy/bin
+    set -gx OMARCHY_PATH $HOME/.local/share/omarchy
+    set -gx PATH $OMARCHY_PATH/bin $PATH
+end
+
 if test (uname) = Darwin
     set -gx CPATH /opt/homebrew/include $CPATH
     set -gx HOMEBREW_NO_ANALYTICS 1
