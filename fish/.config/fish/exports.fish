e8991c807ebfc80c753ad78cd16f9958721dc60b
Mark Tran <mark.tran@gmail.com>
2026-05-02 13:46:17 -0700

    chore(fish): add opencode bin to PATH

--- fish/.config/fish/exports.fish
+++ fish/.config/fish/exports.fish
@@ -4,7 +4,7 @@ set -gx LESS -R
 set -gx LS_COLORS di=32:fi=0:ln=35:pi=5:so=5:bd=5:cd=5:or=31
 set -gx LSCOLORS cxfxcxdxbxegedabagacad
 set -gx PAGER less
-set -gx PATH ./node_modules/.bin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin
+set -gx PATH ./node_modules/.bin $HOME/.opencode/bin $HOME/.cargo/bin $HOME/bin $HOME/.local/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/sbin
 set -gx _ZO_ECHO 1
 
 if test (uname) = Linux; and test -d $HOME/.local/share/omarchy/bin
