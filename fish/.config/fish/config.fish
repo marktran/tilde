e75e6cc9514500204176f4432c841428b4cf1fc0
Mark Tran <mark.tran@gmail.com>
2026-05-30 15:19:06 -0700

    fix(fish): source local config by absolute path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ source "$HOME/.config/fish/aliases.fish"
 source "$HOME/.config/fish/exports.fish"
 source "$HOME/.config/fish/colors.fish"
 
-test -e local.fish; and source local.fish
+test -e "$HOME/.config/fish/local.fish"; and source "$HOME/.config/fish/local.fish"
 
 zoxide init --cmd j fish | source
 type -q mise; and mise activate fish | source
