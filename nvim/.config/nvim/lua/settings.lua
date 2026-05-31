7a913884a5b1aaae95f93ad0b4f71b4d909bc4f5
Mark Tran <mark.tran@gmail.com>
2026-05-31 13:48:11 -0700

    chore(nvim): disable relative line numbers

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,7 +1,6 @@
 local opt = vim.opt
 
 opt.expandtab = true
-opt.relativenumber = true
 opt.ruler = false
 opt.shiftwidth = 2
 opt.showcmd = false
@@ -10,6 +9,8 @@ opt.showtabline = 0
 opt.softtabstop = 2
 opt.tabstop = 2
 opt.wrap = false
+opt.number = true
+opt.relativenumber = false
 
 opt.clipboard:append("unnamedplus")
 opt.fillchars:append({ eob = " " })
