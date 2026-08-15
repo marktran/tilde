6ee3a8b6c276a6c9c0041a9b7de7d5f5fbdd2d87
Mark Tran <mark.tran@gmail.com>
2026-08-14 20:54:50 -0700

    feat(common): add mup alias for fish (Omarchy bash-alias parity)

--- nix/files/fish/config.d/aliases.fish
+++ nix/files/fish/config.d/aliases.fish
@@ -6,6 +6,9 @@ abbr --add -- s sesh
 abbr --add -- t tmux
 abbr --add -- vi nvim
 
+# Omarchy-parity: bash-only alias from /usr/share/omarchy/default/bash/aliases.
+alias mup 'MISE_MINIMUM_RELEASE_AGE=0 mise up'
+
 alias c 'calc -d'
 alias ip 'dig +short myip.opendns.com @resolver1.opendns.com'
 alias l ls
